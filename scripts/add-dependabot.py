19b2e91791e474c9f1aa0bb36d7ddf11cb128ad1
Kilian Lieret <kilian.lieret@posteo.de>
2022-11-09 10:15:28 -0500

    ✨ Add dependabot example working

--- scripts/add-dependabot.py
+++ scripts/add-dependabot.py
@@ -4,13 +4,27 @@ from __future__ import annotations
 
 import os
 import shutil
+import sys
 from pathlib import Path
+
+from maintenance.is_fork import is_fork
+from maintenance.util.log import logger
 
 this_dir = Path(__file__).resolve().parent
 cwd = Path(os.getcwd()).resolve()
 
+user, _, repo_name = os.environ["REPOSITORY"].partition("/")
+logger.debug(f"{user = }, {repo_name = }")
+
+
+if is_fork("klieret", repo_name):
+    logger.info(f"{repo_name} is a fork. skipping")
+    sys.exit(0)
 
 if Path(".github/").is_dir():
+    logger.info("Copying dependabot.yml")
     source = this_dir / "dependabot.yml"
     target = cwd / ".github" / "dependabot.yml"
     shutil.copy(source, target)
+else:
+    logger.info("No .github. Skipping")
--- src/maintenance/is_fork.py
+++ src/maintenance/is_fork.py
@@ -4,14 +4,24 @@ import json
 import subprocess
 import sys
 
+from maintenance.util.log import logger
+
 
 def is_fork(user: str, repo_name: str) -> bool:
+    """Return True if repository is a fork"""
+    cmd = ["gh", "api", "--method", "GET", f"/repos/{user}/{repo_name}"]
+    logger.debug(cmd)
     r = subprocess.run(
-        ["gh", "api", "--method", "GET", f"/repos/{user}/{repo_name}"],
+        cmd,
         capture_output=True,
     )
     asdct = json.loads(r.stdout)
-    return asdct["fork"]
+    try:
+        return asdct["fork"]
+    except KeyError:
+        logger.error("Didn't find 'fork' key")
+        logger.debug(f"For reference, here's the whole output: {asdct}")
+        raise
 
 
 if __name__ == "__main__":
