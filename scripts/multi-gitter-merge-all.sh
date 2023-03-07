06566ec9b375f687c7a372c412b494b9e01a9a7e
Kilian Lieret <kilian.lieret@posteo.de>
2023-03-07 10:31:42 -0500

    ✨ Script to merge all pre-commits everywhere

---
+++ scripts/multi-gitter-merge-all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+multi-gitter merge -U klieret --branch "pre-commit-ci-update-config" --merge-type squash
+multi-gitter merge -O dieret --branch "pre-commit-ci-update-config" --merge-type squash
+multi-gitter merge -O hsf-training --branch "pre-commit-ci-update-config" --merge-type squash
+multi-gitter merge -O gnn-tracking --branch "pre-commit-ci-update-config" --merge-type squash
+multi-gitter merge -O clusterking --branch "pre-commit-ci-update-config" --merge-type squash
