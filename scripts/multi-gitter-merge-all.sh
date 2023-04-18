9af7e5641aa28de0df47b542559657c71f18f4f1
Kilian Lieret <kilian.lieret@posteo.de>
2023-04-18 16:28:49 -0400

    ♻️ Loop over organizations

--- scripts/multi-gitter-merge-all.sh
+++ scripts/multi-gitter-merge-all.sh
@@ -10,7 +10,7 @@ BRANCH_NAME="${1:-pre-commit-ci-update-config}"
 echo "Branch name: ${BRANCH_NAME}"
 
 multi-gitter merge -U klieret --branch "${BRANCH_NAME}" --merge-type squash
-multi-gitter merge -O dieret --branch "${BRANCH_NAME}" --merge-type squash
-multi-gitter merge -O hsf-training --branch "${BRANCH_NAME}" --merge-type squash
-multi-gitter merge -O gnn-tracking --branch "${BRANCH_NAME}" --merge-type squash
-multi-gitter merge -O clusterking --branch "${BRANCH_NAME}" --merge-type squash
+
+for org in dieret hsf-training gnn-tracking clusterking cidakima HSF; do
+    multi-gitter merge -O "${org}" --branch "${BRANCH_NAME}" --merge-type squash
+done
