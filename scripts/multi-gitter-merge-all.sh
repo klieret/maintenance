5ac13fa773d54b4a4ae1af71ca4925e30a63c084
Kilian Lieret <kilian.lieret@posteo.de>
2023-09-04 09:59:40 +0200

    Add merge as additional merge strategy

--- scripts/multi-gitter-merge-all.sh
+++ scripts/multi-gitter-merge-all.sh
@@ -9,8 +9,8 @@ set -x
 BRANCH_NAME="${1:-pre-commit-ci-update-config}"
 echo "Branch name: ${BRANCH_NAME}"
 
-multi-gitter merge -U klieret --branch "${BRANCH_NAME}" --merge-type squash
+multi-gitter merge -U klieret --branch "${BRANCH_NAME}" --merge-type squash,merge
 
 for org in dieret hsf-training gnn-tracking clusterking cidakima HSF; do
-    multi-gitter merge -O "${org}" --branch "${BRANCH_NAME}" --merge-type squash
+    multi-gitter merge -O "${org}" --branch "${BRANCH_NAME}" --merge-type squash,merge
 done
